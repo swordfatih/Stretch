9836059340acd5c62cffd6150aeaaaef4641e9ca
swordfatih <accfldekur@gmail.com>
2022-02-24 11:23:37 +0100

    variables

--- include/stretch/Evaluation.hpp
+++ include/stretch/Evaluation.hpp
@@ -1,9 +1,17 @@
+#include <optional>
 #include "stretch/Arithmetique.hpp"
 
 namespace pe = tao::pegtl;
 
+struct Retour {
+    std::optional< std::vector<Variable> > valeurs;
+};
+
 template <typename Rule, typename Noeud>
-void evaluer(std::unique_ptr<Noeud>& noeud) {
+Retour evaluer(std::unique_ptr<Noeud>& noeud);
+
+template <typename Noeud>
+Retour evaluer<stretch::grammaire>(std::unique_ptr<Noeud>& noeud) {
     if(noeud->type == pe::demangle<stretch::operation>()) {
         evaluer<stretch::operation>(noeud);
     } else if(noeud->type == pe:demangle<stretch::condition>()) {
@@ -12,13 +20,12 @@ void evaluer(std::unique_ptr<Noeud>& noeud) {
 }
 
 template <typename Noeud>
-int evaluer<stretch::operation>(std::unique_ptr<Noeud>& noeud) {
+Retour evaluer<stretch::operation>(std::unique_ptr<Noeud>& noeud) {
     return noeud->children.empty() ? std::stoi(noeud->string()) : stretch::arithmetique::operation<int>(noeud->type, evaluer(noeud->children[0]), evaluer(noeud->children[1]));
 }
 
 template <typename Noeud>
-int evaluer<stretch::condition>(std::unique_ptr<Noeud>& noeud) {
-    
-
-    return evaluer(noeud->);
+Retour evaluer<stretch::condition>(std::unique_ptr<Noeud>& noeud) {
+    if(evaluer(noeud->children[0]) != 0) 
+        evaluer(noeud->children[1]);
 }
