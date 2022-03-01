1f6d0f43e8dfbe79bcc38f9168de5feb59a5c3c3
swordfatih <accfldekur@gmail.com>
2022-03-01 12:09:05 +0100

    action test

--- src/stretch/main.cpp
+++ src/stretch/main.cpp
@@ -7,7 +7,7 @@ namespace pe = tao::pegtl;
 int main()
 {
     std::string filename = "main.st";
-    pe::file_input in(filename);
+    pe::file_input in(filename); 
 
     auto root = pe::parse_tree::parse<stretch::grammaire, stretch::selector>(in);
 
