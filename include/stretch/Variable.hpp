907a54bd56ba83e8df5c5832690c4e2d9838d974
swordfatih <accfldekur@gmail.com>
2022-03-01 23:21:49 +0100

    Ajout de la classe Variable

--- include/stretch/Variable.hpp
+++ include/stretch/Variable.hpp
@@ -1,4 +1,6 @@
-#include <cstdint>
+/////////////////////////////////////////////////
+/// Headers
+/////////////////////////////////////////////////
 #include <variant>
 #include <string>
 #include <memory>
@@ -15,6 +17,9 @@ namespace pe = tao::pegtl;
 
 namespace stretch {
 
+/////////////////////////////////////////////////
+/// @brief Les différents types de valeur
+/////////////////////////////////////////////////
 enum Nature {
     Nul = -1,
     Chaine,
@@ -22,30 +27,57 @@ enum Nature {
     Reel
 };
 
+/////////////////////////////////////////////////
+/// @brief Les types utilisés en mémoire pour
+/// représenter les différents types de valeurs
+/////////////////////////////////////////////////
 using VariantValeur = std::variant< std::string, bool, BigDecimal >;
 
 /////////////////////////////////////////////////
 /// @brief Classe représentant une variable
 ///
-/// Elle doit pouvoir créer une variable depuis:
-/// - La nature et la valeur
+/// Une variable doit pouvoir être créée depuis
+/// - Sa nature et sa valeur
 /// - Un std::variant
-/// - Un std::string
+/// - Parse depuis un std::string
 /// - Un noeud
 /////////////////////////////////////////////////
-struct Variable
+class Variable
 {
 public:
+    /////////////////////////////////////////////////
+    /// @brief Convertir un string en type
+    ///
+    /// @param type La chaîne à convertir
+    /// @return L'énumération correspondante
+    /////////////////////////////////////////////////
     static Nature sto_nature(std::string_view type);
+
+    /////////////////////////////////////////////////
+    /// @brief Convertir un string en valeur
+    ///
+    /// @param type La chaîne à convertir
+    /// @return La valeur correspondante
+    /////////////////////////////////////////////////
     static VariantValeur sto_valeur(Nature type, std::string valeur);
+
+    /////////////////////////////////////////////////
+    /// @brief Créer une variable à partir d'un string
+    ///
+    /// @param type La chaîne à convertir
+    /// @return La variable correspondante
+    /////////////////////////////////////////////////
     static Variable parse(std::string valeur);
 
+    // Constructeurs à partir du type et de la valeur
     Variable(Nature type = Nature::Nul, VariantValeur valeur = {});
     Variable(Nature type, std::string valeur);
 
+    // Constructeurs à partir de la valeur (type déduite)
     Variable(VariantValeur valeur);
     Variable(std::string valeur);
 
+    // Constructeur à partir d'un noeud
     template <typename T>
     Variable(std::unique_ptr<T>& noeud) : Variable(sto_nature(noeud->type), noeud->string()) {}
 
@@ -56,8 +88,8 @@ public:
     friend std::ostream& operator<<(std::ostream& o, const Variable& v) { return o << v.to_string(); }
 
 private:
-    Nature m_type; 
-    VariantValeur m_valeur; 
+    Nature m_type;          ///< Le type de la variable
+    VariantValeur m_valeur; ///< La valeur de la variable
 };
 
 } // namespace stretch
