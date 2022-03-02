f3f71ba249753ab27aaee2191a7b806fbbe83145
GuillaumeMZ <@GuillaumeMZ>
2022-03-02 16:38:06 +0100

    avancées sur la grammaire

--- include/stretch/Grammaire.hpp
+++ include/stretch/Grammaire.hpp
@@ -18,11 +18,14 @@ struct tant_que : pe::istring< 't', 'a', 'n', 't', ' ', 'q', 'u', 'e' > {};
 struct fois : pe::istring< 'f', 'o', 'i', 's' > {};
 struct sortir : pe::istring< 's', 'o', 'r', 't', 'i', 'r' > {};
 struct faire : pe::istring< 'f', 'a', 'i', 'r', 'e' > {};
+struct pour_chaque : pe::istring<'p', 'o', 'u', 'r', ' ', 'c', 'h', 'a', 'q', 'u', 'e'> {};
+struct continuer : pe::istring<'c', 'o', 'n', 't', 'i', 'n', 'u', 'e', 'r'> {};
 
 /// Comparateurs
 struct plus_grand_que : pe::istring< 'p', 'l', 'u', 's', ' ', 'g', 'r', 'a', 'n', 'd', ' ', 'q', 'u', 'e' > {};
 struct plus_petit_que : pe::istring< 'p', 'l', 'u', 's', ' ', 'p', 'e', 't', 'i', 't', ' ', 'q', 'u', 'e' > {};
-struct egal : pe::istring< 'e', 'g', 'a', 'l' > {};
+struct egal : pe::sor<pe::istring< 'e', 'g', 'a', 'l' >, pe::istring<'e', 's', 't'>> {};
+struct different : pe::sor<pe::istring<'d', 'i', 'f', 'f', 'e', 'r', 'e', 'n', 't'>, pe::istring<'n', '\'', 'e', 's', 't', ' ', 'p', 'a', 's'>> {};
 
 /// Conditions
 struct si : pe::istring< 's', 'i' > {};
@@ -43,9 +46,15 @@ struct plus : pe::one< '+' > {};
 struct et : pe::istring< 'e', 't' > {};
 struct ou : pe::istring< 'o', 'u' > {};
 struct non : pe::istring < 'n', 'o', 'n' > {};
-struct est : pe::istring< 'e', 's', 't' > {};
 struct vrai : pe::istring< 'v', 'r', 'a', 'i' > {};
 struct faux : pe::istring< 'f', 'a', 'u', 'x' > {};
+
+/// Operateur d'indexation
+struct hashtag : pe::one< '#' > {};
+
+/// Operateur d'accès
+
+struct de : pe::istring< 'd', 'e' > {};
 
 /// Fonctions
 struct fonction : pe::istring< 'f', 'o', 'n', 'c', 't', 'i', 'o', 'n' > {};
@@ -62,6 +71,11 @@ struct mono_commentaire : pe::one< '|' > {};
 struct parenthese_ouvrante : pe::one< '(' > {};
 struct parenthese_fermante : pe::one< ')' > {};
 
+
+/// Tableaux
+struct crochet_ouvrant : pe::one<'['> {};
+struct crochet_fermant : pe::one<']'> {};
+
 /// Ponctuation
 struct virgule : pe::one< ',' > {};
 struct point : pe::one< '.' > {};
@@ -72,77 +86,119 @@ struct apostrophe : pe::one< '\'' > {};
 /// @brief Separateurs
 /////////////////////////////////////////////////
 struct espaces : pe::plus< pe::space > {};
-struct commentaire : pe::seq< debut_commentaire, pe::until< fin_commentaire > > {};
+struct commentaire : pe::sor<pe::seq< debut_commentaire, pe::until< fin_commentaire > >, pe::seq<mono_commentaire, pe::until<pe::eol>>> {};
 struct separateur : pe::star< pe::sor < commentaire, espaces, pe::eol >> {}; 
 
 /////////////////////////////////////////////////
 /// @brief Valeurs
 /////////////////////////////////////////////////
-struct variable : pe::identifier {};
+struct expression {}; //TODO à faire
 
-struct entier : pe::plus< pe::seq< pe::digit, pe::star< apostrophe > > > {};    ///< ie. 4'500 
-struct reel : pe::seq< entier, point, pe::opt< entier > > {};                   ///< ie. 4'500.5 
-struct chaine : pe::seq< guillemets, pe::until< guillemets > > {};              ///< ie. "hello"
-struct booleen : pe::sor < vrai, faux > {};                                     ///< ie. vrai
+struct variable : pe::identifier {};
+struct alias : pe::seq<pe::one<'@'>, variable> {};
+
+struct identifieur : pe::sor<alias, variable> {};
+
+struct suite_chiffres : pe::list<pe::digit, pe::star<apostrophe>> {};
+struct nombre : pe::seq<pe::opt<pe::sor<plus, moins>>, suite_chiffres, pe::opt<point, suite_chiffres>> {};           ///< ie. 4'500.5 
+
+struct chaine : pe::seq<guillemets, pe::until<guillemets>> {};              ///< ie. "hello"
+struct booleen : pe::sor <vrai, faux> {};                                     ///< ie. vrai
 
 struct operation;
 struct parentheses : pe::seq< parenthese_ouvrante, operation, parenthese_fermante > {};
 struct valeur : pe::sor< variable, entier, reel, chaine, booleen, parentheses > {};
 
-/////////////////////////////////////////////////
-/// @brief Operations
-/////////////////////////////////////////////////
-struct operation_produit : pe::list< pe::seq< separateur, valeur, separateur >, pe::sor < facteur, fraction, modulo > > {};
-struct operation_somme : pe::list< operation_produit, pe::sor < plus, moins > > {};
-struct operation_ordre : pe::list< operation_somme, pe::sor < plus_grand_que, plus_petit_que > > {};
-struct operation_egal : pe::list< operation_ordre, pe::sor < egal > > {};
-struct operation_et : pe::list< operation_egal, pe::sor < et > > {}; 
-struct operation_ou : pe::list< operation_et, pe::sor < ou > > {};
+struct liste_expression : pe::list<expression, virgule, separateur> {};
 
-struct operation : operation_ou {};
 
-struct rearrange_operation : pe::parse_tree::apply< rearrange_operation > 
-{
-    template< typename Node, typename... States >
-    static void transform( std::unique_ptr< Node >& n, States&&... st )
-    {
-        if( n->children.size() == 1 ) {
-            n = std::move( n->children.back() );
-        }
-        else {
-            n->remove_content();
-            auto& c = n->children;
-            auto r = std::move( c.back() );
-            c.pop_back();
-            auto o = std::move( c.back() );
-            c.pop_back();
-            o->children.emplace_back( std::move( n ) );
-            o->children.emplace_back( std::move( r ) );
-            n = std::move( o );
-
-            transform( n->children.front(), st... );
-        }
-    }
-};
-
-/////////////////////////////////////////////////
-/// @brief Conditions
-/////////////////////////////////////////////////
-struct bloc;
-struct condition : pe::seq< si, separateur, operation, separateur, bloc > {};
+struct tableau : pe::seq<crochet_ouvrant, liste_expression, crochet_fermant> {};
 
 /////////////////////////////////////////////////
 /// @brief Assignation
 /////////////////////////////////////////////////
-struct assignation : pe::seq<variable, separateur, fleche_gauche, separateur, operation> {};
+
+struct affectation : pe::seq<variable, fleche_gauche, expression> {};
+struct assignation : pe::seq<pe::list<identifieur, virgule>, fleche_gauche, pe::list<expression, virgule>> {};
+
+
+/////////////////////////////////////////////////
+/// @brief Objets
+/////////////////////////////////////////////////
+
+struct objet : pe::seq<crochet_ouvrant, pe::list<pe::sor<affectation, objet>, virgule>, crochet_fermant> {};
+
+/////////////////////////////////////////////////
+/// @brief Opérateurs
+/////////////////////////////////////////////////
+
+struct indexation : pe::seq<expression, hashtag, expression> {};
+
+struct acces : pe::seq<expression, de, expression> {};
+
+/////////////////////////////////////////////////
+/// @brief Operateurs
+/////////////////////////////////////////////////
+
+// Opérateurs arithmétiques
+
+struct operation_produit : pe::list< pe::seq< separateur, valeur, separateur >, pe::sor < facteur, fraction, modulo > > {};
+struct operation_somme : pe::list< operation_produit, pe::sor < plus, moins > > {};
+struct operation_ordre : pe::list< operation_somme, pe::sor < plus_grand_que, plus_petit_que > > {};
+
+
+struct operation : operation_ou {};
+
+// Opérateurs de comparaison
+
+struct comparaison_pgq : pe::seq<expression, plus_grand_que, expression> {};
+struct comparaison_ppq : pe::seq<expression, plus_petit_que, expression> {};
+struct comparaison_egal : pe::seq<expression, egal, expression> {};
+struct comparaison_different : pe::seq<expression, different, expression> {};
+
+// Opérateurs logiques
+
+struct operateur_et : pe::seq<expression, et, expression> {};
+struct operateur_ou : pe::seq<expression, ou, expression> {};
+struct operateur_non : pe::seq<non, expression> {}};
+
+
+/////////////////////////////////////////////////
+/// @brief Structures de contrôle
+/////////////////////////////////////////////////
 
 /////////////////////////////////////////////////
 /// @brief Blocs d'instructions
 /////////////////////////////////////////////////
+
 struct debut_bloc : pe::sor< pe::bof, faire, alors > {};
 struct fin_bloc : pe::sor< pe::eof, fin > {};
 struct instruction : pe::sor< assignation, condition > {};
 struct bloc : pe::seq< debut_bloc, separateur, pe::star< instruction, separateur >, separateur, pe::until< fin_bloc > > {};
+
+
+// Condition
+
+struct condition_si : pe::seq<si, expression, pe::opt<alors>, bloc, 
+                        pe::star<sinon, si, expression, pe::opt<alors>, bloc>,
+                        pe::opt<sinon, bloc>,
+                        fin> {};
+// Boucles  
+
+struct boucle_tant_que : pe::seq<tant_que, expression, faire, bloc> {};
+struct boucle_repeter : pe::seq<repeter, expression, fois, pe::opt<dans, identifieur>, bloc> {};
+struct boucle_pour_chaque : pe::seq<pour_chaque, identifieur, dans, expression, faire, bloc²> {};
+
+
+/////////////////////////////////////////////////
+/// @brief Fonctions
+/////////////////////////////////////////////////
+
+
+struct definition_fonction : pe::seq<fonction, identifieur, pe::opt<fleche_gauche, liste_expression>, pe::one<':'>, bloc> {};
+struct appel_fonction : pe::seq<identifieur, parenthese_ouvrante, pe::opt<liste_expression>, parenthese_fermante> {};
+
+
 
 /////////////////////////////////////////////////
 /// @brief Grammaire
@@ -181,19 +237,32 @@ using selector = tao::pegtl::parse_tree::selector< Rule,
         operation_produit
     > >;
 
-/////////////////////////////////////////////////
-/// @brief Fonctions
-/////////////////////////////////////////////////
-// struct liste_parametres : pe::list<expression, virgule, separateur> {};
-// struct liste_parametres_opt : pe::opt<liste_parametres> {};
-// struct instruction_retourner : pe::seq<retourner, liste_parametres_opt> {};
-// struct definition_fonction : pe::seq<creer, pe::identifier, pe::opt<fleche_gauche, liste_parametres>, pe::opt<bloc_code>, fin> {}; ///TODO définir instruction (assignement | retourner | quitter | condition | boucle...)
-// struct appel_fonction : pe::seq<pe::identifier, parenthese_ouvrante, liste_parametres_opt, parenthese_fermante> {}; //si la fonction ne prend pas de paramètre l'appel se fait uniquement en écrivant son nom, ça fera bizarre IMO
+/*
 
-/////////////////////////////////////////////////
-/// @brief Boucles
-/////////////////////////////////////////////////
-// struct boucle_tant_que : pe::seq<tant_que, expression, bloc_code, fin> {};
-// struct boucle_repeter_x_fois : pe::seq<repeter, expression, fois, bloc_code, fin> {};
+struct rearrange_operation : pe::parse_tree::apply< rearrange_operation > 
+{
+    template< typename Node, typename... States >
+    static void transform( std::unique_ptr< Node >& n, States&&... st )
+    {
+        if( n->children.size() == 1 ) {
+            n = std::move( n->children.back() );
+        }
+        else {
+            n->remove_content();
+            auto& c = n->children;
+            auto r = std::move( c.back() );
+            c.pop_back();
+            auto o = std::move( c.back() );
+            c.pop_back();
+            o->children.emplace_back( std::move( n ) );
+            o->children.emplace_back( std::move( r ) );
+            n = std::move( o );
+
+            transform( n->children.front(), st... );
+        }
+    }
+};
+
+*/
 
 } // namespace stretch
